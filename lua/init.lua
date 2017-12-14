7509c9c976452e4acf4899ec695985bd290a837e
刘世俊 <liushijun@meituan.com>
2017-12-14 10:09:56 +0800

    change config

--- lua/init.lua
+++ lua/init.lua
@@ -1,7 +1,5 @@
 local waf_path = "/user/local/WAF/"
 local ngxmatch = ngx.re.match
-local ipWhitelist={"127.0.0.1"}
-local ipBlocklist={"10.0.9.98"}
 function getClientIp()
         IP = ngx.var.remote_addr
         if IP == nil then
@@ -24,38 +22,40 @@ function readfile(path)
 end
 
 local forbidden_html = readfile(waf_path.."config/Forbidden.html")
+local ipWhitelist = readfile(waf_path.."config/blocklist")
+local ipBlocklist = readfile(waf_path.."config/whitelist")
 
 function whiteurl()
-if wturlrules ~= nil then
-    for _,rule in pairs(wturlrules) do
-        if ngxmatch(ngx.var.uri, rule, "isjo") then
-            return true
+    if wturlrules ~= nil then
+        for _,rule in pairs(wturlrules) do
+            if ngxmatch(ngx.var.uri, rule, "isjo") then
+                return true
+            end
         end
     end
-end
-return false
+    return false
 end
 
 function whiteip()
-if next(ipWhitelist) ~= nil then
-    for _,ip in pairs(ipWhitelist) do
-        if getClientIp()==ip then
-            return true
+    if next(ipWhitelist) ~= nil then
+        for _,ip in pairs(ipWhitelist) do
+            if getClientIp()==ip then
+                return true
+            end
         end
     end
-end
     return false
 end
 
 function blockip()
- if next(ipBlocklist) ~= nil then
-     for _,ip in pairs(ipBlocklist) do
-         if getClientIp()==ip then
-             ngx.say(forbidden_html)
-             ngx.exit(403)
-             return true
-         end
-     end
- end
-     return false
+    if next(ipBlocklist) ~= nil then
+        for _,ip in pairs(ipBlocklist) do
+            if getClientIp()==ip then
+                ngx.say(forbidden_html)
+                ngx.exit(403)
+            return true
+            end
+        end
+    end
+    return false
 end
